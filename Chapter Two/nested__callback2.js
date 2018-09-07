1b9dcfab3a62615f72a9263bcfa64bb7cba4ee30
20001LastOrder <boqi.chen@mail.mcgill.ca>
2018-09-07 14:40:56 -0400

    added sample code for chapter two

--- Chapter Two/nested__callback2.js
+++ Chapter Two/nested__callback2.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var encoding = 'utf8';
 var dir = './data/';
+var counter = 0;
 
 var writeStream = fs.createWriteStream('./log.txt',
 {
@@ -18,29 +19,46 @@ writeStream.on('open', function(){
 			return;
 		}
 		files.forEach(function(name){
-			//modigy contents
-			fs.readFile(dir + name, encoding, function(err, data){
-				if(err){
-					console.log(err.message);
+			//check if the file is a dir, stat retun object contains information about this
+			fs.stat(dir+name, function(err, stats){
+				if(err) return err;
+				if(!stats.isFile()){
+					console.log(name+' is not a file');
+					counter++;
 					return;
 				}
-				var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
-
-				//write to file
-				fs.writeFile(dir+name, adjData, function(err){
+				//modigy contents
+				fs.readFile(dir + name, encoding, function(err, data){
 					if(err){
 						console.log(err.message);
 						return;
 					}
+					var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
 
-					//log writing
-					writeStream.write('changed'+name+'\n', encoding, function(err){
+					//write to file
+					fs.writeFile(dir+name, adjData, function(err){
 						if(err){
 							console.log(err.message);
+							return;
 						}
+						//log writing
+						writeStream.write('changed 1 '+name+'\n', encoding, function(err){
+							if(err){
+								console.log(err.message);
+								return;
+							}
+							console.log('finished '+name);
+							if(++counter >= files.length){
+								console.log('all done');
+							}
+						});
 					});
 				});
 			});
 		});
 	});
+});
+
+writeStream.on('error', function(err){
+	console.log('ERR: '+err);
 });
